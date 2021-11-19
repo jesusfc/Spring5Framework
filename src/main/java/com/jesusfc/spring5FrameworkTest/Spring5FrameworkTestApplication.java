f6f55f0bb5b0e2abd5d6066b0ca2b3496f23178b
Jesusfc <jfcaraballo@gmail.com>
2021-11-19 15:14:37 +0100

    commit

---
+++ src/main/java/com/jesusfc/spring5FrameworkTest/Spring5FrameworkTestApplication.java
@@ -0,0 +1,13 @@
+package com.jesusfc.spring5FrameworkTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring5FrameworkTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring5FrameworkTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jesusfc/spring5FrameworkTest/Spring5FrameworkTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jesusfc.spring5FrameworkTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring5FrameworkTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
